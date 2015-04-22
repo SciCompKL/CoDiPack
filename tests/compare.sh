4a71f37ba3547f061a1131c7cf2e728c68569ce4
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2015-04-22 12:05:05 +0200

    Compare script has now documentation and argument validation.

--- tests/compare.sh
+++ tests/compare.sh
@@ -1,15 +1,49 @@
 #!/bin/bash
 
+# compare.sh - compare files and dsiplays if they match
+#
+# Author: Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
+# Date:   2015-04-22
+# Category: File Comparison, TXT
+
+# Colored output for 'ok' and 'failure'
 ok="$(tput setaf 2)OK$(tput sgr 0)"
 failure="$(tput setaf 1)FAILURE$(tput sgr 0)"
-cmp $1 $2
-if [ $? -eq 0 ];
-then res1=$ok;
-else res1=$failure;
+
+Usage () {
+    echo >&2 "$0 - compare files with a base file and print out if they match
+usage: $0 [-n name] -b baseFile files ..."
+
+    exit 0
+}
+
+baseFileName=
+testName=
+while getopts n:b: opt
+do
+    case "$opt" in
+      n)  testName=" $OPTARG";;
+      b)  baseFileName="$OPTARG";;
+      \?)   # unknown flag
+          Usage;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+if [ -z $baseFileName ];
+then Usage;
 fi;
-cmp $1 $3
-if [ $? -eq 0 ];
-then res2=$ok;
-else res2=$failure;
-fi;
-echo Test: $4 $res1 $res2
+
+# arguments have been read now iterate over the files and compare them
+res=
+while [ $# -gt 0 ]
+do
+    res+=" "
+    cmp $baseFileName $1
+    if [ $? -eq 0 ];
+    then res+=$ok;
+    else res+=$failure;
+    fi;
+    shift
+done
+echo Test$testName:$res
