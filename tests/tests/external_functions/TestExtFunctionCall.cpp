c0dacee5b7c05519fd825d46ff0824720594ec6b
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2015-05-18 10:27:57 +0200

    Added test for delete functionality.

--- tests/tests/external_functions/TestExtFunctionCall.cpp
+++ tests/tests/external_functions/TestExtFunctionCall.cpp
@@ -1,5 +1,7 @@
 #include <toolDefines.h>
 
+#include <iostream>
+
 IN(2)
 OUT(1)
 POINTS(1) = {{2.0, 3.0}};
@@ -8,25 +10,32 @@ void func_forward(NUMBER& z, const NUMBER& w, const NUMBER& v){
   z = w*v;
 }
 #ifdef CHUNK_TAPE
-static void ext_func(void* checkpoint){
+static void extFunc(void* checkpoint){
   DataStore *check = static_cast<DataStore*>(checkpoint);
   NUMBER *x, w;
   check->getData(x);
-  check->getData(w); 
+  check->getData(w);
   x[0].gradient() += x[1].getValue()*w.getGradient();
-  x[1].gradient() += x[0].getValue()*w.getGradient(); 
+  x[1].gradient() += x[0].getValue()*w.getGradient();
 }
+
+static void delFunc(void* checkpoint){
+  DataStore *check = static_cast<DataStore*>(checkpoint);
+  delete check;
+  std::cout << "Delete" << std::endl;
+}
+
 void func(NUMBER* x, NUMBER* y) {
-  DataStore *Checkpoint = new DataStore;
+  DataStore *checkpoint = new DataStore;
   NUMBER w;
   codi::RealReverse::globalTape.setPassive();
   func_forward(w,x[0],x[1]);
   codi::RealReverse::globalTape.setActive();
 
   codi::RealReverse::globalTape.registerInput(w);
-  Checkpoint->addData(x);
-  Checkpoint->addData(w);
-  codi::RealReverse::globalTape.pushExternalFunction(&ext_func, Checkpoint, NULL);
+  checkpoint->addData(x);
+  checkpoint->addData(w);
+  codi::RealReverse::globalTape.pushExternalFunctionHandle(&extFunc, checkpoint, delFunc);
 
 
   y[0] = w*w;
