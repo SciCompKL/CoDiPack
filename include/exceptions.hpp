c609ca80b4d9af99a6f6e624364ea947fba12ac9
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2015-05-27 08:50:17 +0200

    Added missing namespace to exceptions.

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -26,32 +26,35 @@
 #include <cstdio>
 #include <cstdarg>
 
-/**
- * @brief Generates an exception.
- *
- * @params ...  Arguments for a printf like output and format.
- */
-#define CODI_EXCEPTION(...) outputException( __func__, __FILE__, __LINE__, __VA_ARGS__)
 
-/**
- * @brief Prints the positions and the message of the exception.
- *
- * The position and function where the exceptions occoured is printed. The message will be handled
- * and formatted by a printf like function.
- *
- * @param[in] function  Name of the function from which the exception is generated.
- * @param[in]     file  File where the exception was generated.
- * @param[in]     line  Line inside the file where the exception was generated.
- * @param[in]  message  The exception message and the arguments for the formatting in the message.
- */
-inline void outputException(const char function[], const char file[], const int line, const char* message, ...) {
-  fprintf(stderr, "Error in function %s (%s:%d)\nThe message is:", function, file, line);
+namespace codi {
+  /**
+   * @brief Generates an exception.
+   *
+   * @params ...  Arguments for a printf like output and format.
+   */
+  #define CODI_EXCEPTION(...) outputException( __func__, __FILE__, __LINE__, __VA_ARGS__)
 
-  va_list vl;
-  va_start(vl, message);
-  vfprintf(stderr, message, vl);
-  va_end(vl);
+  /**
+   * @brief Prints the positions and the message of the exception.
+   *
+   * The position and function where the exceptions occoured is printed. The message will be handled
+   * and formatted by a printf like function.
+   *
+   * @param[in] function  Name of the function from which the exception is generated.
+   * @param[in]     file  File where the exception was generated.
+   * @param[in]     line  Line inside the file where the exception was generated.
+   * @param[in]  message  The exception message and the arguments for the formatting in the message.
+   */
+  inline void outputException(const char function[], const char file[], const int line, const char* message, ...) {
+    fprintf(stderr, "Error in function %s (%s:%d)\nThe message is:", function, file, line);
 
-  fprintf(stderr, "\n");
+    va_list vl;
+    va_start(vl, message);
+    vfprintf(stderr, message, vl);
+    va_end(vl);
+
+    fprintf(stderr, "\n");
+  }
 }
 
