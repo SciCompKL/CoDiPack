4209b998f290b0f6451486879c6fd52b573325f8
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2015-06-08 11:01:11 +0200

    Documentation for the data store.

--- include/tools/dataStore.hpp
+++ include/tools/dataStore.hpp
@@ -29,64 +29,136 @@
 
 namespace codi {
 
-  class DeleteHandleBase {
+  /**
+   * @brief Base class the data in the DataStore.
+   *
+   * It just defines a void pointer to the data. The logic is
+   * implemented in the template versions of the class.
+   */
+  class DataHandleBase {
   public:
+    /** @brief The pointer to the data. */
     void* data;
 
-    virtual ~DeleteHandleBase() { };
+    /** @brief Virtual delete function. */
+    virtual ~DataHandleBase() {}
 
-    virtual DeleteHandleBase* clone() = 0;
+    /**
+     * @brief The clone method is used to make a deep copy of the DataStore.
+     * @return A new data object with the same data.
+     */
+    virtual DataHandleBase* clone() = 0;
   };
 
+  /**
+   * @brief Default template implementation for data in the data store.
+   *
+   * The implementation casts the void pointer from the base class to
+   * the given type.
+   *
+   * @tparam The type of the data.
+   */
   template<typename Type>
-  class DeleteHandle : public DeleteHandleBase {
+  class DataHandle : public DataHandleBase {
   public:
-    DeleteHandle(const Type& value) {
+    /**
+     * @brief The constructor will copy the given data.
+     * @param[in] value The data for this object.
+     */
+    DataHandle(const Type& value) {
       data = (void*) new Type(value);
     }
 
-    ~DeleteHandle() {
+    /**
+     * @brief Frees the data object.
+     */
+    ~DataHandle() {
       Type* pointer = (Type*) data;
       delete pointer;
     }
 
-    DeleteHandleBase* clone() { return new DeleteHandle<Type>(*((Type*) data)); }
+    /** @brief Constructs a new data handle with the same data */
+    DataHandleBase* clone() { return new DataHandle<Type>(*((Type*) data)); }
   };
 
+  /**
+   * @brief Default template implementation for array data in the data store.
+   *
+   * The implementation casts the void pointer from the base class to
+   * the given type.
+   *
+   * @tparam The type of the data in the array.
+   */
   template<typename Type>
-  class DeleteHandleArray : public DeleteHandleBase {
+  class DataHandleArray : public DataHandleBase {
   private:
+    /** @brief The size of the array. */
     int size;
   public:
-    DeleteHandleArray(const Type* value, int size) {
+    /**
+     * @brief The constructor will copy the given data.
+     * @param[in] value The data for this object.
+     * @param[in]  size The size of the data array.
+     */
+    DataHandleArray(const Type* value, int size) {
       data = (void*) new Type[size];
       this->size = size;
       std::copy(value, &value[size], (Type*) data);
     }
 
-    ~DeleteHandleArray() {
+    /**
+     * @brief Frees the data object.
+     */
+    ~DataHandleArray() {
       Type* pointer = (Type*) data;
       delete[] pointer;
     }
 
-    DeleteHandleBase* clone() { return new DeleteHandleArray<Type>(*((Type*) data), size); }
+    /** @brief Constructs a new data handle with the same data */
+    DataHandleBase* clone() { return new DataHandleArray<Type>(*((Type*) data), size); }
   };
 
+  /**
+   * @brief The class is used to store data for external functions.
+   *
+   * The DataStore saves the data which is given to it. All the data is copied and
+   * no pointers to the original data are stored.
+   *
+   * The store can also store pointers to the data. Then the users has to take
+   * care that the data is not modified and valid in the instance were the data is
+   * used.
+   *
+   * The order in which the data is read and written to the data store has to be the same.
+   * Otherwise the behaviour is not guaranteed. When all data has been read from the data store
+   * it will reset such that the next call to a getData function will return the first item.
+   */
   class DataStore {
   private:
-    std::vector<DeleteHandleBase*> store;
 
+    /** @brief The vector with the data handles. */
+    std::vector<DataHandleBase*> store;
+
+    /** @brief The current position in the data vector.*/
     size_t storePos;
 
   public:
 
+    /**
+     * @brief Create an empty data store.
+     */
     DataStore() :
       storePos(0) { }
 
+    /**
+     * @brief Deletes all the data handles.
+     */
     ~DataStore() {
       clear();
     }
 
+    /**
+     * @brief Deletes all the data handles.
+     */
     void clear() {
       for(size_t i = 0; i < store.size(); ++i) {
         delete store[i];
@@ -94,6 +166,10 @@ namespace codi {
       store.clear();
     }
 
+    /**
+     * @brief Copy constructor. Creates a deep copy of the data in the data store.
+     * @param[in] other The data store which is cloned.
+     */
     DataStore(const DataStore& other) {
       for(size_t i = 0; i < other.store.size(); ++i) {
         store.push_back(other.store[i]->clone());
@@ -101,6 +177,10 @@ namespace codi {
       storePos = other.storePos;
     }
 
+    /**
+     * @brief Copy operator. Creates a deep copy of the data in the data store.
+     * @param[in] other The data store which is cloned.
+     */
     DataStore& operator=(const DataStore& other) {
       this->clear();
       for(size_t i = 0; i < other.store.size(); ++i) {
@@ -111,21 +191,73 @@ namespace codi {
       return *this;
     }
 
+    /**
+     * @brief Add data to the data store.
+     *
+     * @param[in] value  The data which is stored.
+     *
+     * @tparam Type The type of the stored data.
+     */
     template<typename Type>
     void addData(const Type& value) {
-      store.push_back(new DeleteHandle<Type>(value));
+      store.push_back(new DataHandle<Type>(value));
     }
 
+    /**
+     * @brief Add array data to the data store.
+     *
+     * @param[in] value  The data pointer to the array.
+     * @param[in]  size  The size of the stored data.
+     *
+     * @tparam Type The type of the stored array data.
+     */
     template<typename Type>
     void addData(const Type* value, const int size) {
-      store.push_back(new DeleteHandleArray<Type>(value, size));
+      store.push_back(new DataHandleArray<Type>(value, size));
     }
 
+    /**
+     * @brief Get data from the data store.
+     *
+     * The call to this function has to correspond to
+     * the write call which stored the actual data.
+     *
+     * @param[out] value  The data will be set to this value.
+     *
+     * @tparam Type The type of the extracted data.
+     */
     template<typename Type>
     void getData(Type& value) {
-      getData(&value, 1);
+      getData<Type>(&value, 1);
     }
 
+    /**
+     * @brief Get data from the data store, without copying it.
+     *
+     * The call to this function has to correspond to
+     * the write call which stored the actual data.
+     *
+     * @return The const reference to the data stored.
+     *
+     * @tparam Type The type of the extracted data.
+     */
+    template<typename Type>
+    const Type& getData() {
+      Type* data = nextStore<Type>();
+      return *data;
+    }
+
+    /**
+     * @brief Get data from the data store.
+     *
+     * The call to this function has to correspond to
+     * the write call which stored the actual data.
+     *
+     * @param[out] value  The data will be copied to the value.
+     * @param[in]   size  The size of the data array.
+     *
+     * @tparam Type The type of the extracted data.
+     */
     template<typename Type>
     void getData(Type* value, const int size) {
       Type* convPointer = nextStore<Type>();
@@ -133,11 +265,40 @@ namespace codi {
       std::copy(convPointer, &convPointer[size], value);
     }
 
+    /**
+     * @brief Get array data from the data store, without copying it.
+     *
+     * The call to this function has to correspond to
+     * the write call which stored the actual data.
+     *
+     * @return The const pointer to the data stored.
+     *
+     * @tparam Type The type of the extracted data.
+     */
+    template<typename Type>
+    const Type* getDataArray() {
+      Type* data = nextStore<Type>();
+      return *data;
+    }
+
+    /**
+     * @brief Restart the reading process.
+     */
     void resetPos() {
       storePos = 0;
     }
 
   private:
+    /**
+     * @brief Gets the next data item out of the data vector.
+     *
+     * The call to this function has to correspond to
+     * the write call which stored the actual data.
+     *
+     * @return The pointer to the data stored.
+     *
+     * @tparam Type The type of the extracted data.
+     */
     template<typename Type>
     Type* nextStore() {
       Type* pointer = (Type*) store[storePos++]->data;
