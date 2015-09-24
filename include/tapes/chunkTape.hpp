a27fb3d8fb90bf325a4da386896f3fbd864719b8
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2015-09-24 15:02:25 +0200

    Missing accessor on expression counter.

--- include/tapes/chunkTape.hpp
+++ include/tapes/chunkTape.hpp
@@ -259,7 +259,7 @@ public:
      */
     void allocateAdjoints() {
       //TODO: Tim fragen of er das brauch
-      resizeAdjoints(expressionCount + 1);
+      resizeAdjoints(expressionCount.count + 1);
     }
 
     /**
