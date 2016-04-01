6f817156b3b2c86ad39fda1d51f0648831219d4b
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2016-04-01 11:24:16 +0200

    Added chunk with 3 arrays.

--- include/tapes/chunk.hpp
+++ include/tapes/chunk.hpp
@@ -119,7 +119,7 @@ namespace codi {
    *
    * This chunk contains one data array which is stored in memory.
    *
-   * @tparam Data   The type of the stored data. This type has to be a POD type as we use new, free and memset on it.
+   * @tparam Data   The type of the stored data. This type has to be a POD type as we use malloc, free and memset on it.
    */
   template<typename Data>
   struct Chunk1 : public ChunkInterface {
@@ -183,8 +183,8 @@ namespace codi {
    *
    * This chunk contains two data arrays which are stored in memory.
    *
-   * @tparam Data1   The first type of the stored data. This type has to be a POD type as we use new, free and memset on it.
-   * @tparam Data2   The second type of the stored data. This type has to be a POD type as we use new, free and memset on it.
+   * @tparam Data1   The first type of the stored data. This type has to be a POD type as we use malloc, free and memset on it.
+   * @tparam Data2   The second type of the stored data. This type has to be a POD type as we use malloc, free and memset on it.
    */
   template<typename Data1, typename Data2>
   struct Chunk2 : public ChunkInterface {
@@ -249,6 +249,88 @@ namespace codi {
       codiAssert(index <= ChunkInterface::size);
       pointer1 = &data1[index];
       pointer2 = &data2[index];
+    }
+  };
+
+  /**
+   * @brief Chunk with three data arrays.
+   *
+   * This chunk contains three data arrays which are stored in memory.
+   *
+   * @tparam Data1   The first type of the stored data. This type has to be a POD type as we use malloc, free and memset on it.
+   * @tparam Data2   The second type of the stored data. This type has to be a POD type as we use malloc, free and memset on it.
+   * @tparam Data3   The third type of the stored data. This type has to be a POD type as we use malloc, free and memset on it.
+   */
+  template<typename Data1, typename Data2, typename Data3>
+  struct Chunk3 : public ChunkInterface {
+    Data1* data1; /**< First data item of the chunk */
+    Data2* data2; /**< Second data item of the chunk */
+    Data3* data3; /**< Second data item of the chunk */
+
+    /**
+     * @brief Creates the data of the chunk.
+     *
+     * The data of the chunk is set with memset to zero.
+     *
+     * @param size The size of the data in the chunk.
+     */
+    Chunk3(const size_t& size) : ChunkInterface(size) {
+      data1 = (Data1*)malloc(sizeof(Data1) * size);
+      data2 = (Data2*)malloc(sizeof(Data2) * size);
+      data3 = (Data3*)malloc(sizeof(Data3) * size);
+      if(UseMemsetInChunks) {
+        memset(data1, 0, sizeof(Data1) * size);
+        memset(data2, 0, sizeof(Data2) * size);
+        memset(data3, 0, sizeof(Data3) * size);
+      }
+    }
+
+    /**
+     * @brief Deletes the data arrays
+     */
+    ~Chunk3() {
+      free(data1);
+      free(data2);
+      free(data3);
+      data1 = NULL;
+      data2 = NULL;
+      data3 = NULL;
+    }
+
+    /**
+     * @brief Set the size of the arrays.
+     * @param size  The new size of the arrays.
+     */
+    void resize(const size_t &size) {
+      this->~Chunk3();
+      new (this) Chunk3(size);
+    }
+
+    /**
+     * @brief Set the data values to the current position and increment the used size.
+     * @param value1  The value for the first data array.
+     * @param value2  The value for the second data array.
+     */
+    inline void setDataAndMove(const Data1& value1, const Data2& value2, const Data3& value3) {
+      codiAssert(getUnusedSize() != 0);
+      data1[usedSize] = value1;
+      data2[usedSize] = value2;
+      data3[usedSize] = value3;
+      ++usedSize;
+    }
+
+    /**
+     * @brief Returns a pointer to the data array at the given position.
+     * @param    index  The index in the data array.
+     * @param pointer1  Pointer that is set to the internal data pointer of the first array.
+     * @param pointer2  Pointer that is set to the internal data pointer of the second array.
+     * @return A pointer to the data.
+     */
+    inline void dataPointer(const size_t& index, Data1* &pointer1, Data2* &pointer2, Data3* &pointer3) {
+      codiAssert(index <= ChunkInterface::size);
+      pointer1 = &data1[index];
+      pointer2 = &data2[index];
+      pointer3 = &data3[index];
     }
   };
 
