6f3d3552526ed4eb71f5bba48d724113f09ce0a2
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2017-02-21 00:08:57 +0100

    Added test for tape reset.

---
+++ tests/tests/preaccumulation/TestReset.cpp
@@ -0,0 +1,56 @@
+/**
+ * CoDiPack, a Code Differentiation Package
+ *
+ * Copyright (C) 2015 Chair for Scientific Computing (SciComp), TU Kaiserslautern
+ * Homepage: http://www.scicomp.uni-kl.de
+ * Contact:  Prof. Nicolas R. Gauger (codi@scicomp.uni-kl.de)
+ *
+ * Lead developers: Max Sagebaum, Tim Albring (SciComp, TU Kaiserslautern)
+ *
+ * This file is part of CoDiPack (http://www.scicomp.uni-kl.de/software/codi).
+ *
+ * CoDiPack is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * CoDiPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU
+ * General Public License along with CoDiPack.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Max Sagebaum, Tim Albring, (SciComp, TU Kaiserslautern)
+ */
+
+#include <toolDefines.h>
+
+#include <vector>
+
+IN(2)
+OUT(1)
+POINTS(1) =
+{
+  {  1.0,     0.5}
+};
+
+
+void func(NUMBER* x, NUMBER* y) {
+
+  NUMBER::TapeType& tape = NUMBER::getGlobalTape();
+
+  NUMBER a = x[0] * x[1];
+  NUMBER b = x[0] / sin(x[1]);
+  NUMBER c = b * a;
+
+  NUMBER::TapeType::Position pos = tape.getPosition();
+
+  b = a * x[0];
+
+  tape.reset(pos);
+
+  y[0] = c * a;
+}
