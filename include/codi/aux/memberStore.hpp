03ef02c1d9ec0157b1242ce5001f888933ec6a3c
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2020-03-02 11:05:12 +0100

    Added member store.

---
+++ include/codi/aux/memberStore.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <new>
+#include <utility>
+
+#include "../aux/macros.h"
+#include "../config.h"
+
+/** \copydoc codi::Namespace */
+namespace codi {
+
+  template<typename _Type, typename _Parent, bool _storeStatic = false>
+  struct MemberStore {
+
+      using Type = DECLARE_DEFAULT(_Type, ANY);
+      using Parent = DECLARE_DEFAULT(_Parent, ANY);
+
+      static bool constexpr storeStatic = _storeStatic;
+
+      Type member;
+
+      template<typename ... Args>
+      MemberStore(Args&& ... args) : member(std::forward<Args>(args)...) {}
+
+      Type& get() {
+        return member;
+      }
+
+      Type const& get() const {
+        return member;
+      }
+  };
+
+  template<typename _Type, typename _Parent>
+  struct MemberStore<_Type, _Parent, true> {
+
+      using Type = DECLARE_DEFAULT(_Type, ANY);
+      using Parent = DECLARE_DEFAULT(_Parent, ANY);
+
+      static bool constexpr storeStatic = true;
+
+      static char member[sizeof(Type)];
+      static bool isInitialized;
+
+      template<typename ... Args>
+      MemberStore(Args&& ... args) {
+        if(!isInitialized) {
+          isInitialized = true;
+          new(member) Type(std::forward<Args>(args)...);
+        }
+      }
+
+      Type& get() {
+        return *((Type*)MemberStore::member);
+      }
+
+      Type const& get() const {
+        return *((Type const*)MemberStore::member);
+      }
+  };
+
+  template<typename Type, typename Parent>
+  char MemberStore<Type, Parent, true>::member[sizeof(Type)] = {};
+  template<typename Type, typename Parent>
+  bool MemberStore<Type, Parent, true>::isInitialized = false;
+}
