65c3f882fceea29ed3a07080f9e459fb4ffe80cf
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2020-03-04 18:24:01 +0100

    Added constant expression detection and maximum number of constant arguments value.

--- include/codi/traits/expressionTraits.hpp
+++ include/codi/traits/expressionTraits.hpp
@@ -12,6 +12,8 @@ namespace codi {
   template<typename _Real, typename _Gradient, typename _Tape, typename _Impl>
   struct LhsExpressionInterface;
 
+  template<typename _Real>
+  struct ConstantExpression;
 
   /*******************************************************************************
    * Section: Detection of specific node types
@@ -41,6 +43,18 @@ namespace codi {
   template<typename Impl>
   using enableIfLhsExpression = typename std::enable_if<isLhsExpression<Impl>::value>::type;
 
+  template<typename Impl>
+  struct IsConstantExpression : std::false_type {};
+
+  template<typename Real>
+  struct IsConstantExpression<ConstantExpression<Real>> : std::true_type {};
+
+  template<typename Impl>
+  using isConstantExpression = IsConstantExpression<Impl>;
+
+  template<typename Impl>
+  using enableIfConstantExpression = typename std::enable_if<isConstantExpression<Impl>::value>::type;
+
   /*******************************************************************************
    * Section: Static values on expressions
    *
@@ -59,5 +73,18 @@ namespace codi {
         return 1;
       }
       using CompileTimeTraversalLogic<size_t, MaxNumberOfActiveArguments>::term;
+  };
+
+  template<typename Expr>
+  struct MaxNumberOfConstantArguments : public CompileTimeTraversalLogic<size_t, MaxNumberOfConstantArguments<Expr>> {
+    public:
+
+      static size_t constexpr value = MaxNumberOfConstantArguments::template eval<Expr>();
+
+      template<typename Node, typename = enableIfConstantExpression<Node>>
+      CODI_INLINE static constexpr size_t term() {
+        return 1;
+      }
+      using CompileTimeTraversalLogic<size_t, MaxNumberOfConstantArguments>::term;
   };
 }
