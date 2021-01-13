f34cca1b0362787c0ba41fd14076302504595e0c
Johannes Blühdorn <johannes.bluehdorn@scicomp.uni-kl.de>
2021-01-13 17:26:43 +0100

    Add preaccumulation helper and related Jacobian.

--- include/codi/tools/data/jacobian.hpp
+++ include/codi/tools/data/jacobian.hpp
@@ -38,12 +38,12 @@ namespace codi {
       CODI_INLINE size_t getM() const {return m;} ///< \copydoc codi::JacobianInterface::getM()
       CODI_INLINE size_t getN() const {return n;} ///< \copydoc codi::JacobianInterface::getN()
 
-      /// \copydoc codi::JacobianInterface::operator()( size_t const, size_t const) const
+      /// \copydoc codi::JacobianInterface::operator()(size_t const, size_t const) const
       CODI_INLINE T operator()(size_t const i, size_t const j) const {
         return values[computeIndex(i,j)];
       }
 
-      /// \copydoc codi::JacobianInterface::operator()( size_t const, size_t const)
+      /// \copydoc codi::JacobianInterface::operator()(size_t const, size_t const)
       CODI_INLINE T& operator()(size_t const i, size_t const j) {
         return values[computeIndex(i,j)];
       }
@@ -64,8 +64,106 @@ namespace codi {
     protected:
 
       ///< Compute index into the storage array.
-      CODI_INLINE size_t computeIndex(const size_t i, const size_t j) const {
+      CODI_INLINE size_t computeIndex(size_t const i, size_t const j) const {
         return i * n + j;
       }
   };
+
+
+  template <typename Impl>
+  struct DelayAccessor {
+    private:
+      size_t i;
+      size_t j;
+
+      Impl& data;
+
+    public:
+
+      DelayAccessor(size_t const i, size_t const j, Impl& data) : i(i), j(j), data(data) {}
+
+      template<typename T>
+      DelayAccessor& operator =(T const& v) {
+        data.setLogic(i,j, v);
+
+        return *this;
+      }
+
+      operator typename Impl::T() const {
+        return const_cast<Impl const&>(data).operator()(i,j);
+      }
+  };
+
+
+  template <typename _T, typename _Store = std::vector<_T>>
+  struct JacobianCountNonZerosRow : public Jacobian<_T, _Store> {
+    public:
+
+      std::vector<int> nonZerosRow;
+
+    public:
+      using T = typename Jacobian<_T, _Store>::T; ///< See Jacobian
+      using Store = typename Jacobian<_T, _Store>::Store; ///< See Jacobian
+
+      using DelayAcc = DelayAccessor<JacobianCountNonZerosRow>;
+
+      /** \copydoc Jacobian::Jacobian */
+      explicit JacobianCountNonZerosRow(size_t m, size_t n) : Jacobian<T, Store>(m, n), nonZerosRow(m) {}
+
+
+      /** \copydoc Jacobian::operator()(size_t const  i, size_t const j) const */
+      CODI_INLINE T operator()(size_t const i, size_t const j) const {
+        return Jacobian<T, Store>::operator()(i, j);
+      }
+
+      /**
+       * @brief Returns an object for the delayed access. This object calls then the access logic which updates the
+       * number of non zero elements.
+       *
+       * \copydetails Jacobian::operator()(size_t const i, size_t const j)
+       */
+      CODI_INLINE DelayAcc operator()(size_t const i, size_t const j) {
+        return DelayAcc(i, j, *this);
+      }
+
+      /** \copydoc Jacobian::resize */
+      CODI_INLINE void resize(size_t const m, size_t const n) {
+
+        Jacobian<T, Store>::resize(m, n);
+        nonZerosRow.resize(m);
+      }
+
+      /** \copydoc Jacobian::reshape */
+      CODI_INLINE void reshape(size_t const m, size_t const n) {
+        Jacobian<T, Store>::reshape(m, n);
+        if (nonZerosRow.size() < m) {
+          nonZerosRow.resize(m);
+        }
+      }
+      /**
+       * @brief Reference to the number of non zero entries for the specified row.
+       *
+       * @param[in] i  Output value position. Range: [0, m)
+       *
+       * @return Reference to the non zero count for the specified row.
+       */
+      CODI_INLINE int& nonZeroRow(size_t const i) {
+        return nonZerosRow[i];
+      }
+
+      /**
+       * @brief Checks if the element is non zero and updates the counter.
+       *
+       * @param[in] i  Default
+       * @param[in] j  Default
+       * @param[in] v  Default
+       */
+      CODI_INLINE void setLogic(size_t const i, size_t const j, T const& v) {
+        if (T() != v) {
+          nonZerosRow[i] += 1;
+        }
+        Jacobian<T, Store>::operator()(i, j) = v;
+      }
+  };
+
 }
---
+++ include/codi/tools/helpers/preaccumulationHelper.hpp
@@ -0,0 +1,303 @@
+
+#pragma once
+
+#include <vector>
+
+#include "../algorithms.hpp"
+#include "../data/jacobian.hpp"
+#include "../../config.h"
+#include "../../aux/exceptions.hpp"
+#include "../../expressions/lhsExpressionInterface.hpp"
+#include "../../tapes/interfaces/fullTapeInterface.hpp"
+#include "../../traits/gradientTraits.hpp"
+#include "../../traits/tapeTraits.hpp"
+
+/** \copydoc codi::Namespace */
+namespace codi {
+
+  template<typename _Type, typename = void>
+  struct PreaccumulationHelper {
+    public:
+
+      using Type = CODI_DECLARE_DEFAULT(_Type, CODI_TEMPLATE(LhsExpressionInterface<double, double, CODI_ANY, CODI_ANY>));
+
+      using Real =  typename Type::Real;
+      using Identifier = typename Type::Identifier;
+      using Gradient = typename Type::Gradient;
+
+      using Tape = CODI_DECLARE_DEFAULT(typename Type::Tape, CODI_TEMPLATE(FullTapeInterface<double, double, int, CODI_ANY>));
+      using Position = typename Tape::Position;
+
+      std::vector<Identifier> inputData;
+      std::vector<Identifier> outputData;
+      std::vector<Type*> outputValues;
+      Position startPos;
+
+      std::vector<Gradient> storedAdjoints;
+
+      JacobianCountNonZerosRow<Real> jacobie;
+
+      PreaccumulationHelper() : inputData(), outputData(), outputValues(), startPos(), storedAdjoints(), jacobie(0,0) {}
+
+      template<typename... Inputs>
+      void addInput(Inputs const& ... inputs) {
+        Tape& tape = Type::getGlobalTape();
+
+        if (tape.isActive()) {
+          addInputRec(inputs...);
+        }
+      }
+
+      template<typename ... Inputs>
+      void start(Inputs const& ... inputs) {
+        Tape& tape = Type::getGlobalTape();
+
+        if (tape.isActive()) {
+          inputData.clear();
+          outputData.clear();
+          outputValues.clear();
+
+          startPos = tape.getPosition();
+
+          addInputRec(inputs...);
+        }
+      }
+
+      template<typename ... Outputs>
+      void addOutput(Outputs& ... outputs) {
+        Tape& tape = Type::getGlobalTape();
+
+        if (tape.isActive()) {
+          addOutputRec(outputs...);
+        }
+      }
+
+      template<typename ... Outputs>
+      void finish (bool const storeAdjoints, Outputs& ... outputs) {
+        Tape& tape = Type::getGlobalTape();
+
+        if (tape.isActive()) {
+
+          addOutputRec(outputs...);
+
+          if (storeAdjoints) {
+            storeInputAdjoints();
+          }
+
+          doPreaccumulation();
+
+          if (storeAdjoints) {
+            restoreInputAdjoints();
+          }
+        }
+      }
+
+    private:
+
+      void addInputLogic(Type const& input) {
+        Identifier const& identifier = input.getIdentifier();
+        if (0 != identifier) {
+          inputData.push_back(identifier);
+        }
+      }
+
+      /** @brief terminator for the recursive implementation */
+      void addInputRec() {
+        // terminator implementation
+      }
+
+      template<typename ... Inputs>
+      void addInputRec(Type const& input, Inputs const& ... r) {
+        addInputLogic(input);
+        addInputRec(r...);
+      }
+
+      void addOutputLogic(Type& output) {
+        Identifier const& identifier = output.getIdentifier();
+        if (0 != identifier) {
+          outputData.push_back(identifier);
+          outputValues.push_back(&output);
+        }
+      }
+
+      /** @brief terminator for the recursive implementation */
+      void addOutputRec() {
+        // terminator implementation
+      }
+
+      template<typename ... Outputs>
+      void addOutputRec(Type& output, Outputs& ... r) {
+        addOutputLogic(output);
+        addOutputRec(r...);
+      }
+
+      void storeInputAdjoints() {
+        Tape& tape = Type::getGlobalTape();
+
+        if (storedAdjoints.size() < inputData.size()) {
+          storedAdjoints.resize(inputData.size());
+        }
+
+        for (size_t i = 0; i < inputData.size(); ++i) {
+          Identifier index = inputData[i];
+          Gradient& adjoint = tape.gradient(index);
+          storedAdjoints[i] = adjoint;
+          adjoint = Gradient();
+        }
+      }
+
+      void restoreInputAdjoints() {
+        Tape& tape = Type::getGlobalTape();
+
+        for (size_t i = 0; i < inputData.size(); ++i) {
+          Identifier index = inputData[i];
+          tape.gradient(index) = storedAdjoints[i];
+        }
+      }
+
+      void doPreaccumulation() {
+        // perform the accumulation of the tape part
+        Tape& tape = Type::getGlobalTape();
+
+        Position endPos = tape.getPosition();
+        if (jacobie.getM() != outputData.size() || jacobie.getN() != inputData.size()) {
+          jacobie.reshape(outputData.size(), inputData.size());
+        }
+
+        Algorithms<Type, false>::computeJacobian(startPos, endPos,
+                                                 inputData.data(), inputData.size(),
+                                                 outputData.data(), outputData.size(),
+                                                 jacobie);
+
+        // store the Jacobi matrix
+        tape.reset(startPos);
+
+        for (size_t curOut = 0; curOut < outputData.size(); ++curOut) {
+
+          Type& value = *outputValues[curOut];
+          if (0 != jacobie.nonZeroRow(curOut)) {
+
+            int nonZerosLeft = jacobie.nonZeroRow(curOut);
+            jacobie.nonZeroRow(curOut) = 0;
+
+            // we need to use here the value of the gradient data such that it is correctly deleted.
+            Identifier lastIdentifier = value.getIdentifier();
+            bool staggeringActive = false;
+            int curIn = 0;
+
+            // push statements as long as there are non zeros left
+            // if there are more than MaxStatementIntValue non zeros, then we need to stagger the
+            // statement pushes
+            while (nonZerosLeft > 0) {
+
+              // calculate the number of Jacobies for this statement
+              int jacobiesForStatement = nonZerosLeft;
+              if (jacobiesForStatement >= (int)MaxStatementIntValue) {
+
+                jacobiesForStatement = MaxStatementIntValue;
+                if (staggeringActive) { /* Space is used up but we need one Jacobi for the staggering */
+                  jacobiesForStatement -= 1;
+                }
+              }
+              nonZerosLeft -= jacobiesForStatement; /* update non zeros so that we know if it is the last round */
+
+              Identifier storedIdentifier = lastIdentifier;
+              tape.storeManual(value.getValue(), lastIdentifier, jacobiesForStatement + (int)staggeringActive);
+              if (staggeringActive) { /* Not the first staggering so push the last output */
+                tape.pushJacobiManual(1.0, 0.0, storedIdentifier);
+              }
+
+              // push the rest of the Jacobies for the statement
+              while (jacobiesForStatement > 0) {
+                if (Real() != (Real)jacobie(curOut, curIn)) {
+                  tape.pushJacobiManual(jacobie(curOut, curIn), 0.0, inputData[curIn]);
+                  jacobiesForStatement -= 1;
+                }
+                curIn += 1;
+              }
+
+              staggeringActive = true;
+            }
+
+            value.getIdentifier() = lastIdentifier; /* now set gradient data for the real output value */
+          } else {
+            // disable tape index since there is no dependency
+            tape.destroyIdentifier(value.value(), value.getIdentifier());
+          }
+        }
+      }
+  };
+
+  /**
+   * Helper implementation of the same interface as the PreaccumulationHelper for forward AD tapes.
+   *
+   * This implementation does nothing in all methods.
+   */
+  struct PreaccumulationHelperNoOpBase {
+
+      /**
+       * @brief Does nothing
+       * @param[in] inputs  Not used
+       * @tparam Inputs  Not used.
+       */
+      template<typename ... Inputs>
+      void addInput(Inputs const& ... inputs) {
+        CODI_UNUSED(inputs...);
+        // do nothing
+      }
+
+      /**
+       * @brief Does nothing
+       * @param[in] inputs  Not used
+       * @tparam Inputs  Not used.
+       */
+      template<typename ... Inputs>
+      void start(Inputs const& ... inputs) {
+        CODI_UNUSED(inputs...);
+        // do nothing
+      }
+
+      /**
+       * @brief Does nothing
+       * @param[in,out] outputs  Not used
+       * @tparam Outputs  Not used.
+       */
+      template<typename ... Outputs>
+      void addOutput(Outputs& ... outputs) {
+        CODI_UNUSED(outputs...);
+        // do nothing
+      }
+
+      /**
+       * @brief Does nothing
+       * @param[in] storeAdjoints  Not used
+       * @param[in,out]   outputs  Not used
+       * @tparam Outputs  Not used.
+       */
+      template<typename ... Outputs>
+      void finish(bool const storeAdjoints, Outputs& ... outputs) {
+        CODI_UNUSED(storeAdjoints, outputs...);
+
+        // do nothing
+      }
+  };
+
+  /**
+   * Helper implementation of the same interface as the PreaccumulationHelper for forward AD tapes.
+   *
+   * This implementation does nothing in all methods.
+   * @tparam Type
+   */
+  template<typename Type>
+  struct PreaccumulationHelper<Type, enableIfForwardTape<typename Type::Tape>>
+      : public PreaccumulationHelperNoOpBase {};
+
+  /**
+   * Helper implementation of the same interface as the PreaccumulationHelper for double types.
+   *
+   * This implementation does nothing in all methods.
+   */
+  template<>
+  struct PreaccumulationHelper<double, void>
+      : public PreaccumulationHelperNoOpBase {};
+}
