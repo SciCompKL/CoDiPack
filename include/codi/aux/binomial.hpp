3d0b23797e52a71370fbe19e71e3746c6c08c138
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2021-02-26 11:38:36 +0100

    Clange format skip.

--- include/codi/aux/binomial.hpp
+++ include/codi/aux/binomial.hpp
@@ -20,11 +20,13 @@ namespace codi {
    * @return Binomial coefficient.
    */
   CODI_INLINE size_t constexpr binomial(size_t n, size_t k) {
+    // clang-format off
     return
         k == 0 ? 1 : (
         n < k  ? 0 : ( // outside of the domain we assume zero values
         n == k ? 1 : (
         /* default */ binomial(n - 1, k - 1) + binomial(n - 1, k)
         )));
+    // clang-format on
   }
 }
