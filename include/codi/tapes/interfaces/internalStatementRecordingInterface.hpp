509fdb69383c14770174aec639aa53b5558c999d
Johannes Blühdorn <johannes.bluehdorn@scicomp.uni-kl.de>
2021-04-26 07:10:35 +0200

    Review internalStatementRecordingInterface.hpp.

--- include/codi/tapes/interfaces/internalStatementRecordingInterface.hpp
+++ include/codi/tapes/interfaces/internalStatementRecordingInterface.hpp
@@ -11,27 +11,27 @@ namespace codi {
    *
    * See \ref TapeInterfaces for a general overview of the tape interface design in CoDiPack.
    *
-   * The interface is the callback interface of values in the program to the tape implementation. Each value in the
-   * program needs to allocate an identifier and this identifier needs to be initialized with a call to
-   * initIdentifier(). When the value in the program is destroyed the identifier freed by the tape before it is
-   * destroyed by the program. That is destroyIdentifier() needs to be called before the value is destructed.
+   * This interface contains callbacks used by AD variables to access the tape implementation. Each AD variable in the
+   * program allocates an identifier and this identifier has to be initialized with a call to initIdentifier(). When an
+   * AD variable in the program is destroyed, its identifier has to be freed by the tape by a call to
+   * destroyIdentifier() before it is deallocated.
    *
-   * The compile time switch AllowJacobianOptimization signals the values that the underlying tape is a Jacobian tape
-   * that certain operations can be hidden from the tape recording process.
+   * The compile time switch AllowJacobianOptimization signals the AD variables that the underlying tape is a Jacobian
+   * tape, indicating that certain operations can be hidden from the tape recording process.
    *
-   * store() needs to be called every time by the value when it is assigned. The left hand side value (lhs) needs to
-   * implement the LhsExpressionInterface, the right hand side value (rhs) needs to implement the ExpressionInterface.
+   * store() has to be called by the AD variable every time it is assigned. The left hand side value (lhs) has to
+   * implement LhsExpressionInterface, the right hand side value (rhs) has to implement ExpressionInterface.
    *
    * ActiveType is the default implementation in CoDiPack which uses this interface and implements the behavior
    * described above.
    *
-   * @tparam _Identifier  The adjoint/tangent identification of a tape usually defined by ActiveType::Identifier.
+   * @tparam _Identifier  The adjoint/tangent identification type of a tape, usually chosen as ActiveType::Identifier.
    */
   template<typename _Identifier>
   struct InternalStatementRecordingInterface {
     public:
 
-      using Identifier = CODI_DD(_Identifier, int);  ///< See InternalStatementRecordingInterface
+      using Identifier = CODI_DD(_Identifier, int);  ///< See InternalStatementRecordingInterface.
 
       /*******************************************************************************/
       /// @name Interface definition
@@ -41,19 +41,19 @@ namespace codi {
 
       template<typename Real>
       void initIdentifier(Real& value,
-                          Identifier& identifier);  ///< Needs to be called for each identifier, after it is allocated.
+                          Identifier& identifier);  ///< Has to be called for each identifier, after it is allocated.
       template<typename Real>
-      void destroyIdentifier(Real& value, Identifier& identifier);  ///< Needs to be called for each identifier, before
+      void destroyIdentifier(Real& value, Identifier& identifier);  ///< Has to be called for each identifier, before
                                                                     ///< it is deallocated.
 
       /**
-       * @brief Needs to called by a value every time it is assigned.
+       * @brief Has to called by an AD variable every time it is assigned.
        *
-       * Update of the value is performed by the tape. The tape will additionally store information for the e.g.
+       * Update of the value is performed by the tape. The tape will additionally store information, e.g., for the
        * reversal of the statement.
        *
-       * @tparam Lhs  Needs to implement LhsExpressionInterface.
-       * @tparam Rhs  Needs to implement ExpressionInterface.
+       * @tparam Lhs  Has to implement LhsExpressionInterface.
+       * @tparam Rhs  Has to implement ExpressionInterface.
        */
       template<typename Lhs, typename Rhs>
       void store(Lhs& lhs, Rhs const& rhs);
