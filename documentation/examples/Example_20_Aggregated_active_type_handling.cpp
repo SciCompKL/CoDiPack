180c80e805e6163617fc8cc80a249cb8c4734bee
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2021-06-24 16:24:09 +0200

    Update for Example 20.

--- documentation/examples/Example_20_Aggregated_active_type_handling.cpp
+++ documentation/examples/Example_20_Aggregated_active_type_handling.cpp
@@ -21,21 +21,28 @@ template<typename Type>
 void extFunc_rev(Tape* t, void* d, codi::VectorAccessInterface<RealBase, Identifier>* va) {
   codi::ExternalFunctionUserData* data = (codi::ExternalFunctionUserData*)d;
 
+  // Step 3: Create a wrapped vector access interface.
   using Factory = codi::AggregatedTypeVectorAccessWrapperFactory<Type>;
-  using VectorWrapper = typename Factory::RType;                    // Step 2: Create a wrapped vector access interface.
-
+  using VectorWrapper = typename Factory::RType;
   VectorWrapper* vaType = Factory::create(va);
 
   using TypeIdentifier = typename VectorWrapper::Identifier;
   using TypeReal = typename VectorWrapper::Real;
 
-  TypeIdentifier t_i = data->getData<TypeIdentifier>();
+  // Step 4: Get the external function data
+  TypeReal x_v = data->getData<TypeReal>();
+  TypeIdentifier x_i = data->getData<TypeIdentifier>();
+  TypeIdentifier w_i = data->getData<TypeIdentifier>();
 
-  TypeReal t_b = vaType->getAdjoint(t_i, 0);                        // Step 3: Use the wrapped vector access interface.
+  // Step 5: Use the wrapped vector access interface and perform the adjoint operation
+  TypeReal w_b = vaType->getAdjoint(w_i, 0);
+  TypeReal t_b = 2.0 * codi::ComputationTraits::transpose(x_v) * w_b;
 
-  std::cout << " Reverse: t_b = " << t_b  << std::endl;
+  vaType->updateAdjoint(x_i, 0, t_b);
+  vaType->resetAdjoint(w_i, 0);
 
-  Factory::destroy(vaType);                                         // Step 4: Delete the created wrapper.
+  // Step 6: Delete the created wrapper.
+  Factory::destroy(vaType);
 }
 
 void extFunc_del(Tape* t, void* d) {
@@ -47,42 +54,50 @@ void extFunc_del(Tape* t, void* d) {
 }
 
 template<typename Type>
-void addExternalFunc(Type const& v) {
+Type addExternalFunc(Type const& x) {
 
   Tape& tape = Real::getGlobalTape();
 
+  // Step 1: Perform the passive function evaluation.
+  tape.setPassive();
+  Type w = func(x);
+  tape.setActive();
+  codi::RealTraits::registerExternalFunctionOutput(w);
+
+  // Step 2: Use the general access routines on the values to extract the primal and identifier data.
   codi::ExternalFunctionUserData* data = new codi::ExternalFunctionUserData();
-  data->addData(codi::RealTraits::getIdentifier(v));                // Step 1: Use the general access routines an values of the type.
+  data->addData(codi::RealTraits::getValue(x));
+  data->addData(codi::RealTraits::getIdentifier(x));
+  data->addData(codi::RealTraits::getIdentifier(w));
 
   tape.pushExternalFunction(codi::ExternalFunction<Tape>::create(extFunc_rev<Type>, data, extFunc_del));
+
+  return w;
 }
 //! [Typed external function]
 
 int main(int nargs, char** args) {
-  Real x = 4.0;
+  Real x = 3.0;
 
   Tape& tape = Real::getGlobalTape();
   tape.setActive();
 
   tape.registerInput(x);
-  Real t = func(x);
+  Real t1 = addExternalFunc(x);
 
-  addExternalFunc(t);
+  std::complex<Real> c(t1, -t1);
+  std::complex<Real> t2 = addExternalFunc(c);
 
-  std::complex<Real> c(t, -t);
-  std::complex<Real> w = func(c);
-
-  addExternalFunc(w);
-
-  Real y = func(std::abs(w));
+  Real y = std::abs(t2);
   tape.registerOutput(y);
 
   tape.setPassive();
   y.setGradient(1.0);
   tape.evaluate();
 
-  std::cout << "f(4.0) = " << y << std::endl;
-  std::cout << "df/dx(4.0) = " << x.getGradient() << std::endl;
+  std::cout << "x = " << x << std::endl;
+  std::cout << "y = " << y << std::endl;
+  std::cout << "dy/dx = " << x.getGradient() << std::endl;
 
   tape.reset();
 
--- documentation/examples/Example_20_Aggregated_active_type_handling.md
+++ documentation/examples/Example_20_Aggregated_active_type_handling.md
@@ -1,7 +1,7 @@
-Example 20 - External function type data extraction {#Example_20_Aggregated_active_type_handling}
+Example 20 - Aggregated active type handling {#Example_20_Aggregated_active_type_handling}
 =======
 
-**Goal:** Learn how to use generalize the data extraion for external functions.
+**Goal:** Learn how to generalize the data extraction for external functions.
 
 **Prequesties:** \ref Example_11_External_function_user_data
 
@@ -10,3 +10,8 @@ Example 20 - External function type data extraction {#Example_20_Aggregated_acti
 
 **Full code:**
 \snippet examples/Example_20_Aggregated_active_type_handling.cpp Example 20 - Aggregated active type handling
+
+
+In the primal application the helper structure from codi::RealTraits are used. For the reverse handling in the external
+function the codi::AggregatedTypeVectorAccessWrapperFactory is used to create a wrapped version of the
+codi::VectorAccessInterface. In addition codi::ComputationTraits are used for a generlization of the transpose.
