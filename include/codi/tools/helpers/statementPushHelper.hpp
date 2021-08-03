d43bf93d9360bd898a4f7ea5b82cddf491d15525
Johannes Blühdorn <johannes.bluehdorn@scicomp.uni-kl.de>
2021-08-03 15:43:38 +0200

    Review tools/helpers/statementPushHelper.hpp.

--- include/codi/tools/helpers/statementPushHelper.hpp
+++ include/codi/tools/helpers/statementPushHelper.hpp
@@ -10,24 +10,24 @@
 namespace codi {
 
   /**
-   * @brief Base class for manual statement pushed on the tape.
+   * @brief Base class for manual statement pushes on the tape.
    *
    * See StatementPushHelper for details.
    *
-   * @tparam _Type  The CoDiPack type on which tape the statements are pushed.
-   * @tparam _Impl  The actual implementation
+   * @tparam _Type  The CoDiPack type on whose tape the statements are pushed.
+   * @tparam _Impl  The actual implementation.
    */
   template<typename _Type, typename _Impl>
   struct StatementPushHelperBase {
     public:
 
-      /// See StatementPushHelperBase
+      /// See StatementPushHelperBase.
       using Type = CODI_DD(_Type, CODI_T(LhsExpressionInterface<double, double, CODI_ANY, CODI_ANY>));
 
-      /// See StatementPushHelperBase
+      /// See StatementPushHelperBase.
       using Impl = CODI_DD(_Impl, CODI_T(StatementPushHelperBase<CODI_ANY, CODI_ANY>));
 
-      using Real = typename Type::Real;  ///< See LhsExpressionInterface
+      using Real = typename Type::Real;  ///< See LhsExpressionInterface.
 
       /*******************************************************************************/
       /// @name Interface definition
@@ -36,7 +36,7 @@ namespace codi {
       /// Finish the push of a statement. Performs lhs = primal and cleans up all data.
       void endPushStatement(Type& lhs, Real const& primal);
 
-      /// Add the Jacobian of an argument for the statement.
+      /// Add the Jacobian of an argument of the statement.
       void pushArgument(Type const& arg, Real const& jacobian);
 
       /// Initialize all data for the push of a statement.
@@ -90,31 +90,31 @@ namespace codi {
   /**
    * @brief Add statements to the tape where the Jacobians are computed manually.
    *
-   * This helper class can be used to optimize the storage of a statement or to handle involved functions, that can not
+   * This helper class can be used to optimize the storage of a statement or to handle involved functions that cannot
    * be handled with CoDiPack.
    *
    * The three basic use cases are:
    * \snippet examples/statementPushHelper.cpp Statement push helper
    *
-   * After a statement is pushed the helper can be used again for the next statement.
+   * After a statement is pushed, the helper can be used again for the next statement.
    *
-   * @tparam _Type  The CoDiPack type on which tape the statements are pushed.
+   * @tparam _Type  The CoDiPack type on whose tape the statements are pushed.
    */
   template<typename _Type, typename = void>
   struct StatementPushHelper : public StatementPushHelperBase<_Type, StatementPushHelper<_Type>> {
     public:
 
-      ///< See StatementPushHelper
+      ///< See StatementPushHelper.
       using Type = CODI_DD(_Type, CODI_T(LhsExpressionInterface<double, double, CODI_ANY, CODI_ANY>));
 
-      using Real = typename Type::Real;              ///< See LhsExpressionInterface
-      using Identifier = typename Type::Identifier;  ///< See LhsExpressionInterface
-      /// See LhsExpressionInterface
+      using Real = typename Type::Real;              ///< See LhsExpressionInterface.
+      using Identifier = typename Type::Identifier;  ///< See LhsExpressionInterface.
+      /// See LhsExpressionInterface.
       using Tape = CODI_DD(typename Type::Tape, CODI_T(FullTapeInterface<double, double, int, CODI_ANY>));
 
     protected:
-      Identifier indexData[Config::MaxArgumentSize];  ///< Storage for the identifiers of the arguments
-      Real jacobianData[Config::MaxArgumentSize];     ///< Storage for the Jacobians of the arguments
+      Identifier indexData[Config::MaxArgumentSize];  ///< Storage for the identifiers of the arguments.
+      Real jacobianData[Config::MaxArgumentSize];     ///< Storage for the Jacobians of the arguments.
       size_t dataPos;                                 ///< Current number of arguments.
 
     public:
@@ -177,15 +177,15 @@ namespace codi {
       : public StatementPushHelperBase<_Type, StatementPushHelper<_Type>> {
     public:
 
-      /// See StatementPushHelper
+      /// See StatementPushHelper.
       using Type = CODI_DD(_Type, CODI_T(LhsExpressionInterface<double, double, CODI_ANY, CODI_ANY>));
 
-      using Real = typename Type::Real;          ///< See LhsExpressionInterface
-      using Gradient = typename Type::Gradient;  ///< See LhsExpressionInterface
+      using Real = typename Type::Real;          ///< See LhsExpressionInterface.
+      using Gradient = typename Type::Gradient;  ///< See LhsExpressionInterface.
 
     protected:
 
-      Gradient lhsTangent;  ///< Tangent value for the left hand side
+      Gradient lhsTangent;  ///< Tangent value for the left hand side.
 
     public:
 
@@ -214,13 +214,13 @@ namespace codi {
       /// @}
   };
 
-  /// Specialization for double
+  /// Specialization for double.
   template<>
   struct StatementPushHelper<double, void> {
     public:
 
-      using Type = double;  ///< See LhsExpressionInterface
-      using Real = double;  ///< See LhsExpressionInterface
+      using Type = double;  ///< See LhsExpressionInterface.
+      using Real = double;  ///< See LhsExpressionInterface.
 
       /*******************************************************************************/
       /// @name Implementation of StatementPushHelperBase and overwrite of all methods.
