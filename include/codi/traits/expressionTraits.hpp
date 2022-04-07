2c9212aa5dcade72d4fb161ce9d841bd865dbf91
Max Sagebaum <max.sagebaum@scicomp.uni-kl.de>
2022-04-07 10:16:48 +0200

    Expression trait for the number of operations.

--- include/codi/traits/expressionTraits.hpp
+++ include/codi/traits/expressionTraits.hpp
@@ -224,6 +224,28 @@ namespace codi {
     bool constexpr numberOfConstantTypeArguments = NumberOfConstantTypeArguments<Expr>::value;
 #endif
 
+    /// Counts the number of nodes in the expression.
+    template<typename Expr>
+    struct NumberOfOperations
+        : public CompileTimeTraversalLogic<size_t, NumberOfOperations<Expr>> {
+      public:
+
+        /// \copydoc CompileTimeTraversalLogic::node()
+        template<typename Node>
+        CODI_INLINE static size_t constexpr node() {
+          return 1 + NumberOfOperations::template toLinks<Node>();
+        }
+
+        /// See NumberOfOperations
+        static size_t constexpr value = NumberOfOperations::template eval<Expr>();
+    };
+
+#if CODI_IS_CPP14
+    /// Value entry of NumberOfOperations
+    template<typename Expr>
+    bool constexpr numberOfOperations = NumberOfOperations<Expr>::value;
+#endif
+
     /// @}
   }
 }
